b7db4d3bed4e43c0551083d54896665227c942bf
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-12-17 14:44:58 -0800

    feat: Add HasParser class (#8)

--- src/VCard/Parse.hs
+++ src/VCard/Parse.hs
@@ -2,10 +2,13 @@
 -- SPDX-License-Identifier: BSD-3-Clause
 
 -- | Support for parsing VCards
-module VCard.Parse (Parser) where
+module VCard.Parse (Parser, HasParser (..)) where
 
 import Data.Text (Text)
 import Data.Void (Void)
 import Text.Megaparsec (Parsec)
 
 type Parser = Parsec Void Text
+
+class HasParser a where
+  parser :: Parser a
