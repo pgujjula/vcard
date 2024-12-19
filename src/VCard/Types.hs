61956bf6b76947e52d075c4cc23c7dde35ed11c2
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-12-18 21:07:21 -0800

    feat: Add marshalling for Value{Param,Type} (#5)
    Add a parser/serializer for ValueParam and ValueType.

--- src/VCard/Types.hs
+++ src/VCard/Types.hs
@@ -1,7 +1,9 @@
 -- SPDX-FileCopyrightText: Copyright Preetham Gujjula
 -- SPDX-License-Identifier: BSD-3-Clause
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module VCard.Types
   ( VCard (..),
@@ -9,18 +11,22 @@ module VCard.Types
     FN (..),
     Version (..),
     ValueParam (..),
-    ValueType (..)
+    ValueType (..),
+    XName (..),
   )
 where
 
 import Control.Monad (void)
+import Control.Monad.Combinators (choice, (<|>))
 import Control.Monad.Combinators.NonEmpty qualified as NonEmpty
+import Data.Char (isAlphaNum, isAscii)
 import Data.Function ((&))
+import Data.Functor (($>))
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty qualified as NonEmpty
 import Data.Text (Text, pack)
 import Data.Text qualified as Text
-import Text.Megaparsec (takeWhileP)
+import Text.Megaparsec (takeWhile1P, takeWhileP)
 import Text.Megaparsec.Char (string)
 import VCard.Parse (HasParser (..), Parser)
 import VCard.Serialize (HasSerializer (..), Serializer)
@@ -103,6 +109,17 @@ instance HasSerializer FN where
 newtype ValueParam = ValueParam {unValueParam :: ValueType}
   deriving (Eq, Show, Ord)
 
+instance HasParser ValueParam where
+  parser :: Parser ValueParam
+  parser = do
+    void (string "VALUE=")
+    ValueParam <$> parser @ValueType
+
+instance HasSerializer ValueParam where
+  serializer :: Serializer ValueParam
+  serializer (ValueParam valueType) =
+    "VALUE=" <> serializer valueType
+
 data ValueType
   = VTText
   | VTURI
@@ -119,5 +136,54 @@ data ValueType
   | VTXName XName
   deriving (Eq, Show, Ord)
 
+instance HasParser ValueType where
+  parser :: Parser ValueType
+  parser =
+    choice
+      [ string "text" $> VTText,
+        string "uri" $> VTURI,
+        string "date" $> VTDate,
+        string "time" $> VTTime,
+        string "date-time" $> VTDateTime,
+        string "date-and-or-time" $> VTDateAndOrTime,
+        string "timestamp" $> VTTimestamp,
+        string "boolean" $> VTBoolean,
+        string "integer" $> VTInteger,
+        string "float" $> VTFloat,
+        string "utc-offset" $> VTUTCOffset,
+        string "language-tag" $> VTLanguageTag,
+        VTXName <$> parser
+      ]
+
+instance HasSerializer ValueType where
+  serializer :: Serializer ValueType
+  serializer = \case
+    VTText -> "text"
+    VTURI -> "uri"
+    VTDate -> "date"
+    VTTime -> "time"
+    VTDateTime -> "date-time"
+    VTDateAndOrTime -> "date-and-or-time"
+    VTTimestamp -> "timestamp"
+    VTBoolean -> "boolean"
+    VTInteger -> "integer"
+    VTFloat -> "float"
+    VTUTCOffset -> "utc-offset"
+    VTLanguageTag -> "language-tag"
+    VTXName xName -> serializer xName
+
 newtype XName = XName {unXName :: Text}
   deriving (Eq, Show, Ord)
+
+instance HasParser XName where
+  parser :: Parser XName
+  parser = do
+    void (string "x-" <|> string "X-")
+    xNameText <-
+      takeWhile1P Nothing $ \c ->
+        (isAscii c && isAlphaNum c) || c == '-'
+    pure (XName xNameText)
+
+instance HasSerializer XName where
+  serializer :: Serializer XName
+  serializer xName = "x-" <> unXName xName
