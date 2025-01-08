efffa8481dc2017feb285b75f605f2cf33855fb2
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-01-08 13:53:12 -0800

    test: Add unit tests for `text-list` type (#6)

--- test/Test/VCard/Types/Value/Text.hs
+++ test/Test/VCard/Types/Value/Text.hs
@@ -8,14 +8,16 @@ module Test.VCard.Types.Value.Text (tests) where
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.List.NonEmpty qualified as NE
 import Data.Text qualified
+import Data.Text qualified as Text
 import Test.Tasty (TestName, TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 import VCard.Parse (parse)
 import VCard.Serialize (serialize)
-import VCard.Types.Value.Text (Text (..))
+import VCard.Types.Value.List (List (..))
+import VCard.Types.Value.Text (Text (..), TextList)
 
 tests :: TestTree
-tests = testGroup "Text" [textTests]
+tests = testGroup "Text" [textTests, textListTests]
 
 --
 -- Text
@@ -57,3 +59,44 @@ invalidTexts =
     ("ascii_control_character_bell", "Doc\BELSportello"),
     ("crlf_space", "Doc\r\n Sportello")
   ]
+
+--
+-- TextList
+--
+textListTests :: TestTree
+textListTests = testGroup "TextList" [validTextListTests, invalidTextListTests]
+
+validTextListTests :: TestTree
+validTextListTests =
+  testGroup "valid" $ flip map validTextLists $ \(name, text, value) ->
+    testCase name $ do
+      parse text @?= Just value
+      serialize value @?= text
+
+validTextLists :: [(TestName, Data.Text.Text, TextList)]
+validTextLists = individualLists ++ [combinedList]
+  where
+    individualLists = flip map (NE.toList validTexts) $ \(name, text, value) ->
+      ( name,
+        "Bigfoot," <> text <> ",Shasta",
+        List (Text "Bigfoot" :| [value, Text "Shasta"])
+      )
+
+    combinedList =
+      let combinedText :: Data.Text.Text
+          combinedText =
+            Text.intercalate "," $
+              map (\(_, text, _) -> text) (NE.toList validTexts)
+
+          combinedValue :: TextList
+          combinedValue = List (NE.map (\(_, _, value) -> value) validTexts)
+       in ("all_combined", combinedText, combinedValue)
+
+invalidTextListTests :: TestTree
+invalidTextListTests =
+  testGroup "invalid" $ flip map invalidTextLists $ \(name, text) ->
+    testCase name (parse @Text text @?= Nothing)
+
+invalidTextLists :: [(TestName, Data.Text.Text)]
+invalidTextLists = flip map invalidTexts $ \(name, text) ->
+  (name, "Bigfoot," <> text <> ",Shasta")
