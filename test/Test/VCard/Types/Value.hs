12af9307cc48e6db385a77bf7d6e8c1ab076fa8e
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-01-08 18:20:36 -0800

    test: Add property tests for `uri` type (#7)
    Add tests to ensure that URI parsing and serializing works as expected.
    Since the URI type itself is imported from network-uri, we don't need to
    make the testing very robust. Instead, we simply add sanity checks to
    ensure that the megaparsec-based URI parser behaves as expected,
    especially when there are stray leading or trailing characters.

--- test/Test/VCard/Types/Value.hs
+++ test/Test/VCard/Types/Value.hs
@@ -5,10 +5,12 @@ module Test.VCard.Types.Value (tests) where
 
 import Test.Tasty (TestTree, testGroup)
 import Test.VCard.Types.Value.Text qualified
+import Test.VCard.Types.Value.URI qualified
 
 tests :: TestTree
 tests =
   testGroup
     "Value"
-    [ Test.VCard.Types.Value.Text.tests
+    [ Test.VCard.Types.Value.Text.tests,
+      Test.VCard.Types.Value.URI.tests
     ]
---
+++ test/Test/VCard/Types/Value/URI.hs
@@ -0,0 +1,79 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.VCard.Types.Value.URI (tests) where
+
+import Data.GenValidity.URI (genURI)
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.QuickCheck (choose, discard, elements, testProperty, (===))
+import Text.Megaparsec (parseMaybe)
+import Text.Megaparsec.Char (string)
+import VCard.Parse (parse, parser)
+import VCard.Serialize (serialize)
+import VCard.Types.Value.URI (URI)
+
+tests :: TestTree
+tests = testGroup "URI" [validURITests, invalidURITests]
+
+validURITests :: TestTree
+validURITests = testGroup "valid" [completeURITests, prefixURITests]
+
+completeURITests :: TestTree
+completeURITests = testProperty "parse_entire_string" $ do
+  uriText <- Text.pack . show <$> genURI
+  pure $ fmap serialize (parse @URI uriText) === Just uriText
+
+prefixURITests :: TestTree
+prefixURITests = testProperty "parse_prefix" $ do
+  uriText <- Text.pack . show <$> genURI
+  suffix <- elements nonURITexts
+  let fullText = uriText <> suffix
+  pure $
+    fmap serialize (parseMaybe (parser @URI <* string suffix) fullText)
+      === Just uriText
+
+invalidURITests :: TestTree
+invalidURITests =
+  testGroup
+    "invalid"
+    [ strayPrefixTests,
+      strayInfixCharTests,
+      straySuffixCharTests
+    ]
+
+strayPrefixTests :: TestTree
+strayPrefixTests = testProperty "stray_prefix" $ do
+  uriText <- Text.pack . show <$> genURI
+  prefix <- elements nonURITexts
+  let invalidURI = prefix <> uriText
+  pure $ parseMaybe (parser @URI) invalidURI === Nothing
+
+strayInfixCharTests :: TestTree
+strayInfixCharTests = testProperty "stray_infix" $ do
+  uriText <- Text.pack . show <$> genURI
+  infix' <- elements nonURITexts
+  if Text.length uriText < 2
+    then pure discard
+    else do
+      middleIndex <- choose (1, Text.length uriText - 1)
+      let invalidURI =
+            Text.take middleIndex uriText
+              <> infix'
+              <> Text.drop middleIndex uriText
+      pure $ parseMaybe (parser @URI) invalidURI === Nothing
+
+straySuffixCharTests :: TestTree
+straySuffixCharTests = testProperty "stray_suffix" $ do
+  uriText <- Text.pack . show <$> genURI
+  suffix <- elements nonURITexts
+  let invalidURI = uriText <> suffix
+  pure $ parseMaybe (parser @URI) invalidURI === Nothing
+
+-- Characters/character sequences that cannot appear in URIs
+nonURITexts :: [Text]
+nonURITexts = ["\n", " ", "\r\n", "\""]
