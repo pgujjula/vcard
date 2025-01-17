99c04979a390baf594afd2012292e8a744a4d8c0
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-01-16 17:37:25 -0800

    refactor: Reorder date tests (1/2)
    Reorder the tests in Test.VCard.Types.Value.Date so that the order of
    the tests corresponds to the order of the exports in VCard.Types.Value,
    namely Year, Month, Day, YearMonthDay, YearMonth, MonthDay.
    
    Split the reorder into two different commits so that the diffs are
    rendered nicely.

--- test/Test/VCard/Types/Value/Date.hs
+++ test/Test/VCard/Types/Value/Date.hs
@@ -47,96 +47,13 @@ tests :: TestTree
 tests =
   testGroup
     "Date"
-    [ dayTests,
-      monthTests,
+    [ monthTests,
       yearTests,
+      dayTests,
       yearMonthDayTests,
       yearMonthTests,
       monthDayTests
     ]
-
---
--- Day
---
-dayTests :: TestTree
-dayTests = testGroup "Day" [validDayTests, invalidDayTests, dayBoundsTests]
-
-validDayTests :: TestTree
-validDayTests = testCase "valid" $ do
-  forM_ validDays $ \(text, value) -> do
-    parse text @?= Just value
-    serialize value @?= text
-
-validDays :: [(Text, Day)]
-validDays = singleDigits ++ doubleDigits
-  where
-    singleDigits :: [(Text, Day)]
-    singleDigits = flip map [1 .. 9] $ \i ->
-      ("0" <> showt i, Day (finite (i - 1)))
-
-    doubleDigits :: [(Text, Day)]
-    doubleDigits = flip map [10 .. 31] $ \i ->
-      (showt i, Day (finite (i - 1)))
-
-invalidDayTests :: TestTree
-invalidDayTests =
-  testCase "invalid" $
-    forM_ invalidDays $ \text -> do
-      parse @Day text @?= Nothing
-
-dayBoundsTests :: TestTree
-dayBoundsTests = testCase "bounds" $ do
-  minBound @?= Day (finite 0)
-  maxBound @?= Day (finite 30)
-
-invalidDays :: [Text]
-invalidDays =
-  [ "0",
-    "00",
-    "000",
-    "0000",
-    "00\n",
-    "1",
-    "001",
-    "0001",
-    "01\n",
-    "7",
-    "007",
-    "0007",
-    "07\n",
-    "030",
-    "0030",
-    "30\n",
-    "031",
-    "0031",
-    "31\n",
-    "32",
-    "032",
-    "0032",
-    "32\n",
-    "40",
-    "040",
-    "0040",
-    "40\n",
-    "-1",
-    "-01",
-    "-001",
-    "-0001",
-    "-1\n",
-    "-01\n",
-    "-12",
-    "-012",
-    "-0012",
-    "-12\n",
-    "-20",
-    "-020",
-    "-0020",
-    "-20\n",
-    "a",
-    "1a",
-    "a1",
-    "a\n"
-  ]
 
 --
 -- Month
@@ -282,6 +199,89 @@ yearBoundsTests :: TestTree
 yearBoundsTests = testCase "bounds" $ do
   minBound @?= Year (finite 0)
   maxBound @?= Year (finite 9999)
+
+--
+-- Day
+--
+dayTests :: TestTree
+dayTests = testGroup "Day" [validDayTests, invalidDayTests, dayBoundsTests]
+
+validDayTests :: TestTree
+validDayTests = testCase "valid" $ do
+  forM_ validDays $ \(text, value) -> do
+    parse text @?= Just value
+    serialize value @?= text
+
+validDays :: [(Text, Day)]
+validDays = singleDigits ++ doubleDigits
+  where
+    singleDigits :: [(Text, Day)]
+    singleDigits = flip map [1 .. 9] $ \i ->
+      ("0" <> showt i, Day (finite (i - 1)))
+
+    doubleDigits :: [(Text, Day)]
+    doubleDigits = flip map [10 .. 31] $ \i ->
+      (showt i, Day (finite (i - 1)))
+
+invalidDayTests :: TestTree
+invalidDayTests =
+  testCase "invalid" $
+    forM_ invalidDays $ \text -> do
+      parse @Day text @?= Nothing
+
+dayBoundsTests :: TestTree
+dayBoundsTests = testCase "bounds" $ do
+  minBound @?= Day (finite 0)
+  maxBound @?= Day (finite 30)
+
+invalidDays :: [Text]
+invalidDays =
+  [ "0",
+    "00",
+    "000",
+    "0000",
+    "00\n",
+    "1",
+    "001",
+    "0001",
+    "01\n",
+    "7",
+    "007",
+    "0007",
+    "07\n",
+    "030",
+    "0030",
+    "30\n",
+    "031",
+    "0031",
+    "31\n",
+    "32",
+    "032",
+    "0032",
+    "32\n",
+    "40",
+    "040",
+    "0040",
+    "40\n",
+    "-1",
+    "-01",
+    "-001",
+    "-0001",
+    "-1\n",
+    "-01\n",
+    "-12",
+    "-012",
+    "-0012",
+    "-12\n",
+    "-20",
+    "-020",
+    "-0020",
+    "-20\n",
+    "a",
+    "1a",
+    "a1",
+    "a\n"
+  ]
 
 --
 -- YearMonthDay
