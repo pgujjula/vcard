b3d650bc746f472b3ce06c53987cce251b9148ba
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-03 21:23:19 -0800

    feat: Add `toScientific`/`fromScientific` (#15)
    Add `toScientific` and `fromScientific` functions for the `float` value
    data type.

--- src/VCard/Types/Value/Float.hs
+++ src/VCard/Types/Value/Float.hs
@@ -11,9 +11,18 @@ module VCard.Types.Value.Float
   ( Float (..),
     FloatList,
     NaturalLeadingZeros (..),
+    toScientific,
+    fromScientific,
   )
 where
 
+import Data.Scientific
+  ( Scientific,
+    base10Exponent,
+    coefficient,
+    normalize,
+    scientific,
+  )
 import Data.Text qualified as Text
 import GHC.Exts (int2Word#, word2Int#)
 import GHC.Int (Int (I#))
@@ -28,6 +37,7 @@ import VCard.Types.Value.Integer (naturalP)
 import VCard.Types.Value.List (List (..))
 import VCard.Types.Value.Time (Sign (..))
 import Prelude hiding (Float, Integer)
+import Prelude qualified (Integer)
 
 data Float = Float
   { floatSign :: !(Maybe Sign),
@@ -71,6 +81,75 @@ instance HasSerializer Float where
     maybe "" serializer (floatSign float)
       <> naturalLeadingZerosS (floatWholePart float)
       <> maybe "" (("." <>) . naturalLeadingZerosS) (floatDecimalPart float)
+
+toScientific :: Float -> Scientific
+toScientific float =
+  let sign :: Scientific
+      sign = case floatSign float of
+        Nothing -> 1.0
+        Just Plus -> 1.0
+        Just Minus -> -1.0
+
+      wholeScientific :: Scientific
+      wholeScientific =
+        scientific
+          (toInteger (naturalLeadingZerosValue (floatWholePart float)))
+          0
+
+      decimalScientific :: Scientific
+      decimalScientific =
+        case floatDecimalPart float of
+          Nothing -> 0
+          Just decimalPart ->
+            let integer = toInteger (naturalLeadingZerosValue decimalPart)
+                exponent' =
+                  word2Int (naturalLeadingZerosNumLeadingZeros decimalPart)
+                    + length (show (naturalLeadingZerosValue decimalPart))
+             in scientific integer (-exponent')
+   in sign * (wholeScientific + decimalScientific)
+
+fromScientific :: Scientific -> Float
+fromScientific s =
+  let sign :: Maybe Sign
+      sign =
+        if s >= 0
+          then Nothing
+          else Just Minus
+
+      s' :: Scientific
+      s' = abs (normalize s)
+
+      wholePart :: Natural
+      wholePart = fromInteger (floor s')
+
+      decimalPart :: Scientific
+      decimalPart = s' - fromIntegral wholePart
+
+      coeff :: Prelude.Integer
+      coeff = coefficient decimalPart
+
+      exponent' :: Int
+      exponent' = base10Exponent decimalPart
+
+      numDecimalLeadingZeros :: Int
+      numDecimalLeadingZeros = (-exponent') - length (show coeff)
+   in Float
+        { floatSign = sign,
+          floatWholePart =
+            NaturalLeadingZeros
+              { naturalLeadingZerosNumLeadingZeros = 0,
+                naturalLeadingZerosValue = wholePart
+              },
+          floatDecimalPart =
+            if decimalPart == 0
+              then Nothing
+              else
+                Just $
+                  NaturalLeadingZeros
+                    { naturalLeadingZerosNumLeadingZeros = int2Word numDecimalLeadingZeros,
+                      naturalLeadingZerosValue = fromInteger coeff
+                    }
+        }
 
 type FloatList = List Float
 
