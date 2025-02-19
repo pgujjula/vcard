b802af90d6975225d705070a26dab71f2deb35a3
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-19 01:21:46 -0800

    feat: Add tools for changing case of `Char` (#188)
    Add type families and singletons for raising and lowering the case of a
    `Char`.

--- src/VCard/Symbol/Private.hs
+++ src/VCard/Symbol/Private.hs
@@ -15,9 +15,25 @@ module VCard.Symbol.Private
     -- * Singleton equality
     testSCharEquality,
     testSSymbolEquality,
+
+    -- * Change case of 'Char'
+
+    -- ** Lower case
+    ToLowerChar,
+    sToLowerChar,
+
+    -- ** Upper case
+    ToUpperChar,
+    sToUpperChar,
   )
 where
 
+import VCard.Symbol.Private.Case
+  ( ToLowerChar,
+    ToUpperChar,
+    sToLowerChar,
+    sToUpperChar,
+  )
 import VCard.Symbol.Private.Compat
   ( SChar,
     SSymbol,
