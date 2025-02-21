39772704ff0b8937817afd126bc54d6ed02ff0d1
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-20 18:36:18 -0800

    test: Add unit tests for `Language` param (#117)

--- test/Test/VCard/Types/Param.hs
+++ test/Test/VCard/Types/Param.hs
@@ -5,10 +5,12 @@ module Test.VCard.Types.Param (tests) where
 
 import Test.Tasty (TestTree, testGroup)
 import Test.VCard.Types.Param.Generic qualified
+import Test.VCard.Types.Param.Language qualified
 
 tests :: TestTree
 tests =
   testGroup
     "Param"
-    [ Test.VCard.Types.Param.Generic.tests
+    [ Test.VCard.Types.Param.Generic.tests,
+      Test.VCard.Types.Param.Language.tests
     ]
---
+++ test/Test/VCard/Types/Param/Language.hs
@@ -0,0 +1,55 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.VCard.Types.Param.Language (tests) where
+
+import Control.Monad (forM_)
+import Data.Text (Text)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+import VCard.CaseInsensitive (CaseInsensitiveUpper (..))
+import VCard.Parse (parse)
+import VCard.Serialize (serialize)
+import VCard.Symbol.Private (symbolSing)
+import VCard.Types.Param.Generic (Param (..))
+import VCard.Types.Param.Language (Language)
+import VCard.Types.Value.LanguageTag (LanguageTag (..))
+
+tests :: TestTree
+tests =
+  testGroup
+    "Language"
+    [ test_parse,
+      test_serialize
+    ]
+
+test_parse :: TestTree
+test_parse =
+  testCase "parse" $ do
+    forM_ units_valid $ \(text, value) -> parse text @?= Just value
+    forM_ units_invalid $ \text -> parse @Language text @?= Nothing
+
+test_serialize :: TestTree
+test_serialize = testCase "serialize" $
+  forM_ units_valid $
+    \(text, value) -> serialize value @?= text
+
+units_valid :: [(Text, Language)]
+units_valid =
+  [ ( "LANGUAGE=en",
+      Param
+        (CaseInsensitiveUpper (symbolSing @"LANGUAGE"))
+        (LanguageTag "en")
+    ),
+    ( "language=EN-gb",
+      Param
+        (CaseInsensitiveUpper (symbolSing @"language"))
+        (LanguageTag "EN-gb")
+    )
+  ]
+
+units_invalid :: [Text]
+units_invalid = ["LANGUAGE="]
