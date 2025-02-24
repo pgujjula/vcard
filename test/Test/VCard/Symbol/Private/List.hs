b99822dbacd603671002416bfe226e035cc0f145
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-24 01:07:00 -0800

    test: Add unit test for `sToList` (#214)

--- test/Test/VCard/Symbol/Private/List.hs
+++ test/Test/VCard/Symbol/Private/List.hs
@@ -7,21 +7,41 @@
 module Test.VCard.Symbol.Private.List (tests) where
 
 import Data.Dynamic (Dynamic, toDyn)
+import Data.Singletons (fromSing)
 import Data.Type.Equality ((:~:) (Refl))
+import GHC.TypeLits (symbolVal)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (testCase)
-import VCard.Symbol.Private.List (ToList)
+import Test.Tasty.HUnit (Assertion, testCase, (@?=))
+import VCard.Symbol.Private (SSymbol, symbolSing, withKnownSymbol)
+import VCard.Symbol.Private.List (ToList, sToList)
 
 tests :: TestTree
 tests =
   testGroup
     "List"
-    [ testCase "ToList" (seq test_Types (pure ()))
+    [ test_ToList,
+      test_sToList
     ]
 
-test_Types :: [Dynamic]
-test_Types =
-  [ toDyn (Refl :: ToList "" :~: '[]),
-    toDyn (Refl :: ToList "a" :~: '[ 'a']),
-    toDyn (Refl :: ToList "Foo Bar" :~: '[ 'F', 'o', 'o', ' ', 'B', 'a', 'r'])
-  ]
+test_ToList :: TestTree
+test_ToList = testCase "ToList" (seq refls (pure ()))
+  where
+    refls :: [Dynamic]
+    refls =
+      [ toDyn (Refl :: ToList "" :~: '[]),
+        toDyn (Refl :: ToList "a" :~: '[ 'a']),
+        toDyn
+          (Refl :: ToList "Foo Bar" :~: '[ 'F', 'o', 'o', ' ', 'B', 'a', 'r'])
+      ]
+
+test_sToList :: TestTree
+test_sToList =
+  testCase "sToList" $ do
+    let testSing :: SSymbol s -> Assertion
+        testSing ss =
+          fromSing (sToList ss) @?= withKnownSymbol ss (symbolVal ss)
+    testSing (symbolSing @"")
+    testSing (symbolSing @"a")
+    testSing (symbolSing @"ab")
+    testSing (symbolSing @"abc")
+    testSing (symbolSing @"Foo Bar")
