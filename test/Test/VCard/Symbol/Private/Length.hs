a48ce33cbc8b16bbe404fbf603dbf639cb3e8576
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-24 14:56:25 -0800

    test: Add unit test for `sLength` (#219)

--- test/Test/VCard/Symbol/Private/Length.hs
+++ test/Test/VCard/Symbol/Private/Length.hs
@@ -1,24 +1,55 @@
 -- SPDX-FileCopyrightText: Copyright Preetham Gujjula
 -- SPDX-License-Identifier: BSD-3-Clause
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Test.VCard.Symbol.Private.Length (tests) where
 
 import Data.Dynamic (Dynamic, toDyn)
+import Data.Proxy (Proxy (..))
 import Data.Type.Equality ((:~:) (Refl))
-import Test.Tasty (TestTree)
-import Test.Tasty.HUnit (testCase)
-import VCard.Symbol.Private.Length (Length)
+import GHC.TypeLits (SomeSymbol (..), natVal, someSymbolVal)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (Assertion, testCase, (@?=))
+import VCard.Natural.Private.Compat (withKnownNat)
+import VCard.Symbol.Private.Compat (symbolSing)
+import VCard.Symbol.Private.Length (Length, sLength)
+
+{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
 
 tests :: TestTree
-tests = testCase "Length" (seq test_Types (pure ()))
+tests =
+  testGroup
+    "Length"
+    [ test_Length,
+      test_sLength
+    ]
 
-test_Types :: [Dynamic]
-test_Types =
+test_Length :: TestTree
+test_Length = testCase "Length" (seq cases_Length (pure ()))
+
+cases_Length :: [Dynamic]
+cases_Length =
   [ toDyn (Refl :: Length "" :~: 0),
     toDyn (Refl :: Length "a" :~: 1),
     toDyn (Refl :: Length "abc" :~: 3),
     toDyn (Refl :: Length "Foo Bar" :~: 7)
   ]
+
+test_sLength :: TestTree
+test_sLength =
+  testCase "sLength" $ do
+    let lengthViaSingleton :: String -> Int
+        lengthViaSingleton xs =
+          case someSymbolVal xs of
+            SomeSymbol (Proxy :: Proxy s) ->
+              let sl = sLength (symbolSing @s)
+               in fromInteger (withKnownNat sl (natVal sl))
+        checkLength :: String -> Assertion
+        checkLength xs = lengthViaSingleton xs @?= length xs
+    checkLength ""
+    checkLength "a"
+    checkLength "Foo Bar"
+    checkLength (replicate 100 'z')
