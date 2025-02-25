2998c9e829c198e5a614022bbc37679aace8595c
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-25 13:34:50 -0800

    test: Add unit tests for `Pref` param (#119)
    Add unit tests for `Pref` param parsing and serializing.

--- test/Test/VCard/Types/Param.hs
+++ test/Test/VCard/Types/Param.hs
@@ -6,6 +6,7 @@ module Test.VCard.Types.Param (tests) where
 import Test.Tasty (TestTree, testGroup)
 import Test.VCard.Types.Param.Generic qualified
 import Test.VCard.Types.Param.Language qualified
+import Test.VCard.Types.Param.Pref qualified
 import Test.VCard.Types.Param.Value qualified
 
 tests :: TestTree
@@ -14,5 +15,6 @@ tests =
     "Param"
     [ Test.VCard.Types.Param.Generic.tests,
       Test.VCard.Types.Param.Language.tests,
+      Test.VCard.Types.Param.Pref.tests,
       Test.VCard.Types.Param.Value.tests
     ]
---
+++ test/Test/VCard/Types/Param/Pref.hs
@@ -0,0 +1,99 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.VCard.Types.Param.Pref (tests) where
+
+import Data.Finite (finite)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+import VCard.CaseInsensitive (CaseInsensitiveUpper (..))
+import VCard.Parse (parse)
+import VCard.Serialize (serialize)
+import VCard.Symbol.Private (symbolSing)
+import VCard.Types.Param.Generic (Param (..))
+import VCard.Types.Param.Pref (Pref, PrefValue (..))
+
+tests :: TestTree
+tests =
+  testGroup
+    "Pref"
+    [ test_parse,
+      test_serialize
+    ]
+
+test_parse :: TestTree
+test_parse =
+  testCase "parse" $ do
+    (parse "PREF=1" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"PREF"),
+            paramValue = PrefValue (finite 0)
+          }
+    (parse "pref=2" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"pref"),
+            paramValue = PrefValue (finite 1)
+          }
+    (parse "Pref=99" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"Pref"),
+            paramValue = PrefValue (finite 98)
+          }
+    (parse "prEf=100" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"prEf"),
+            paramValue = PrefValue (finite 99)
+          }
+    --
+    parse "PREF=0" @?= (Nothing :: Maybe Pref)
+    parse "PREF=01" @?= (Nothing :: Maybe Pref)
+    parse "PREF=002" @?= (Nothing :: Maybe Pref)
+    parse "PREF=0099" @?= (Nothing :: Maybe Pref)
+    parse "PREF=0100" @?= (Nothing :: Maybe Pref)
+    --
+    parse "PREF =5" @?= (Nothing :: Maybe Pref)
+    parse "PREF= 5" @?= (Nothing :: Maybe Pref)
+    parse " PREF=5" @?= (Nothing :: Maybe Pref)
+    parse "PREF=5 " @?= (Nothing :: Maybe Pref)
+    parse "PREF=5;" @?= (Nothing :: Maybe Pref)
+    parse "PREF=5:" @?= (Nothing :: Maybe Pref)
+
+test_serialize :: TestTree
+test_serialize =
+  testCase "serialize" $ do
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"PREF"),
+            paramValue = PrefValue (finite 0)
+          }
+      )
+      @?= "PREF=1"
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"pref"),
+            paramValue = PrefValue (finite 1)
+          }
+      )
+      @?= "pref=2"
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"Pref"),
+            paramValue = PrefValue (finite 98)
+          }
+      )
+      @?= "Pref=99"
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"prEf"),
+            paramValue = PrefValue (finite 99)
+          }
+      )
+      @?= "prEf=100"
