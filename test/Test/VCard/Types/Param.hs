6b2170e1b0dd10095da1850b5329545860323964
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-25 15:31:48 -0800

    test: Add unit tests for `Calscale` param (#124)
    Add unit tests for `Calscale` param parsing and serializing.

--- test/Test/VCard/Types/Param.hs
+++ test/Test/VCard/Types/Param.hs
@@ -4,6 +4,7 @@
 module Test.VCard.Types.Param (tests) where
 
 import Test.Tasty (TestTree, testGroup)
+import Test.VCard.Types.Param.Calscale qualified
 import Test.VCard.Types.Param.Generic qualified
 import Test.VCard.Types.Param.Language qualified
 import Test.VCard.Types.Param.Pref qualified
@@ -13,7 +14,8 @@ tests :: TestTree
 tests =
   testGroup
     "Param"
-    [ Test.VCard.Types.Param.Generic.tests,
+    [ Test.VCard.Types.Param.Calscale.tests,
+      Test.VCard.Types.Param.Generic.tests,
       Test.VCard.Types.Param.Language.tests,
       Test.VCard.Types.Param.Pref.tests,
       Test.VCard.Types.Param.Value.tests
---
+++ test/Test/VCard/Types/Param/Calscale.hs
@@ -0,0 +1,123 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.VCard.Types.Param.Calscale (tests) where
+
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+import VCard.CaseInsensitive
+  ( CaseInsensitiveLower (..),
+    CaseInsensitiveUpper (..),
+  )
+import VCard.Parse (parse)
+import VCard.Serialize (serialize)
+import VCard.Symbol.Private (symbolSing)
+import VCard.Types.Param.Calscale (Calscale, CalscaleValue (..))
+import VCard.Types.Param.Generic (Param (..))
+
+tests :: TestTree
+tests =
+  testGroup
+    "Calscale"
+    [ test_parse,
+      test_serialize
+    ]
+
+test_parse :: TestTree
+test_parse =
+  testCase "parse" $ do
+    (parse "CALSCALE=gregorian" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CALSCALE"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"gregorian"))
+          }
+    (parse "calscale=GREGORIAN" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"calscale"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"GREGORIAN"))
+          }
+    (parse "CalScale=gregOrian" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CalScale"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"gregOrian"))
+          }
+    --
+    (parse "CALSCALE=x-abc" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CALSCALE"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"x-abc"))
+          }
+    (parse "CalScale=X-ABC" @?=) $
+      Just $
+        Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CalScale"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"X-ABC"))
+          }
+
+    parse "CALSCALE =gregorian" @?= (Nothing :: Maybe (Calscale "gregorian"))
+    parse "CALSCALE= gregorian" @?= (Nothing :: Maybe (Calscale "gregorian"))
+    parse " CALSCALE=gregorian" @?= (Nothing :: Maybe (Calscale "gregorian"))
+    parse "CALSCALE=gregorian " @?= (Nothing :: Maybe (Calscale "gregorian"))
+    parse "CALSCALE=gregorian;" @?= (Nothing :: Maybe (Calscale "gregorian"))
+    parse "CALSCALE=gregorian:" @?= (Nothing :: Maybe (Calscale "gregorian"))
+
+    parse "CALSCALE=gregorian" @?= (Nothing :: Maybe (Calscale "x-abc"))
+    parse "CALSCALE=x-abc" @?= (Nothing :: Maybe (Calscale "gregorian"))
+
+test_serialize :: TestTree
+test_serialize =
+  testCase "serialize" $ do
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CALSCALE"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"gregorian"))
+          }
+      )
+      @?= "CALSCALE=gregorian"
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"calscale"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"GREGORIAN"))
+          }
+      )
+      @?= "calscale=GREGORIAN"
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CalScale"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"gregOrian"))
+          }
+      )
+      @?= "CalScale=gregOrian"
+    --
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CALSCALE"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"x-abc"))
+          }
+      )
+      @?= "CALSCALE=x-abc"
+    serialize
+      ( Param
+          { paramName = CaseInsensitiveUpper (symbolSing @"CalScale"),
+            paramValue =
+              CalscaleValue (CaseInsensitiveLower (symbolSing @"X-ABC"))
+          }
+      )
+      @?= "CalScale=X-ABC"
