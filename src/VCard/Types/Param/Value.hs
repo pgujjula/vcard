f232cc410198ebffb175e4e6df33c10c1b0e1aaf
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-25 02:29:14 -0800

    feat: Implement `Value` param (#118)

---
+++ src/VCard/Types/Param/Value.hs
@@ -0,0 +1,49 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+
+module VCard.Types.Param.Value
+  ( Value,
+    ValueValue (..),
+  )
+where
+
+import Data.Constraint (Dict (..))
+import GHC.TypeLits (KnownSymbol, Symbol)
+import VCard.CaseInsensitive (CaseInsensitiveLower)
+import VCard.Parse (HasParser (..), Parser)
+import VCard.Serialize (HasSerializer (..), Serializer)
+import VCard.Symbol.Private (symbolSing)
+import VCard.Types.Param.Generic (Param)
+import VCard.Types.Param.Value.ValueValueSymbol
+  ( ValueValueSymbol,
+    testValueValueSymbol,
+  )
+
+{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
+
+type Value s = Param "VALUE" (ValueValue s)
+
+data ValueValue (s :: Symbol) where
+  ValueValue :: (ValueValueSymbol s) => CaseInsensitiveLower s -> ValueValue s
+
+deriving instance Eq (ValueValue s)
+
+deriving instance Show (ValueValue s)
+
+instance (KnownSymbol s) => HasParser (ValueValue s) where
+  parser :: Parser (ValueValue s)
+  parser =
+    case testValueValueSymbol (symbolSing @s) of
+      Nothing -> fail "no ValueValueSymbol"
+      Just Dict -> ValueValue <$> parser @(CaseInsensitiveLower s)
+
+instance HasSerializer (ValueValue s) where
+  serializer :: Serializer (ValueValue s)
+  serializer (ValueValue x) = serializer x
---
+++ src/VCard/Types/Param/Value/ValueValueSymbol.hs
@@ -0,0 +1,101 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+
+module VCard.Types.Param.Value.ValueValueSymbol
+  ( ValueValueSymbol,
+    testValueValueSymbol,
+  )
+where
+
+import Data.Constraint (Dict (..))
+import Data.Proxy (Proxy (..))
+import Data.Type.Equality ((:~:) (Refl))
+import GHC.TypeLits (Symbol)
+import VCard.Internal.Closed (Closed)
+import VCard.Symbol.Private (SSymbol, symbolSing, testSSymbolEquality)
+import VCard.XName (XNameLowerSymbol, testXNameLowerSymbol)
+
+-- | 'ValueValueSymbol' is a closed class, attempting to define a new instance
+--   will result in a compiler error.
+class ValueValueSymbol (s :: Symbol) where
+  default _privateValueValueSymbol :: (Closed ValueValueSymbol) => Proxy s
+  _privateValueValueSymbol :: Proxy s
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "text" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "uri" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "date" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "time" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "date-time" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "date-and-or-time" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "timestamp" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "boolean" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "integer" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "float" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "utc-offset" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# INCOHERENT #-} ValueValueSymbol "language-tag" where
+  _privateValueValueSymbol = Proxy
+
+instance {-# OVERLAPPABLE #-} (XNameLowerSymbol s) => ValueValueSymbol s where
+  _privateValueValueSymbol = Proxy
+
+testValueValueSymbol :: forall s. SSymbol s -> Maybe (Dict (ValueValueSymbol s))
+testValueValueSymbol ss =
+  testLiteral (symbolSing @"text")
+    . testLiteral (symbolSing @"uri")
+    . testLiteral (symbolSing @"date")
+    . testLiteral (symbolSing @"time")
+    . testLiteral (symbolSing @"date-time")
+    . testLiteral (symbolSing @"date-and-or-time")
+    . testLiteral (symbolSing @"timestamp")
+    . testLiteral (symbolSing @"boolean")
+    . testLiteral (symbolSing @"integer")
+    . testLiteral (symbolSing @"float")
+    . testLiteral (symbolSing @"utc-offset")
+    . testLiteral (symbolSing @"language-tag")
+    $ case testXNameLowerSymbol ss of
+      Nothing -> Nothing
+      Just Dict -> Just Dict
+  where
+    testLiteral ::
+      forall t.
+      (ValueValueSymbol t) =>
+      SSymbol t ->
+      Maybe (Dict (ValueValueSymbol s)) ->
+      Maybe (Dict (ValueValueSymbol s))
+    testLiteral st x =
+      case testSSymbolEquality ss st of
+        Just Refl -> Just (Dict @(ValueValueSymbol t))
+        Nothing -> x
