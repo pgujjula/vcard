4e10edb658ff68d85edf6102a000555f18ca019d
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-03-02 23:02:42 -0800

    test: Add tests for `PID` param (#121)
    Add unit tests for the `PID` param parser and serializer.

--- test/Test/VCard/Types/Param.hs
+++ test/Test/VCard/Types/Param.hs
@@ -8,6 +8,7 @@ import Test.VCard.Types.Param.Calscale qualified
 import Test.VCard.Types.Param.Generic qualified
 import Test.VCard.Types.Param.Geo qualified
 import Test.VCard.Types.Param.Language qualified
+import Test.VCard.Types.Param.PID qualified
 import Test.VCard.Types.Param.Pref qualified
 import Test.VCard.Types.Param.Type qualified
 import Test.VCard.Types.Param.Value qualified
@@ -20,6 +21,7 @@ tests =
       Test.VCard.Types.Param.Generic.tests,
       Test.VCard.Types.Param.Geo.tests,
       Test.VCard.Types.Param.Language.tests,
+      Test.VCard.Types.Param.PID.tests,
       Test.VCard.Types.Param.Pref.tests,
       Test.VCard.Types.Param.Value.tests,
       Test.VCard.Types.Param.Type.tests
---
+++ test/Test/VCard/Types/Param/PID.hs
@@ -0,0 +1,176 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.VCard.Types.Param.PID (tests) where
+
+import Control.Monad (forM_)
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List.NonEmpty qualified as NonEmpty
+import Data.Proxy (Proxy (..))
+import Data.Text (Text)
+import Test.Tasty (TestName, TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+import VCard.CaseInsensitive (CaseInsensitiveUpper (..))
+import VCard.Parse (HasParser, parse)
+import VCard.Serialize (HasSerializer, serialize)
+import VCard.Symbol.Private (symbolSing)
+import VCard.Types.Param.Generic (Param (..))
+import VCard.Types.Param.PID (PID, PIDValue (..))
+
+{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
+
+tests :: TestTree
+tests =
+  testGroup
+    "PID"
+    [ test_parse,
+      test_serialize
+    ]
+
+test_parse :: TestTree
+test_parse =
+  testGroup
+    "parse"
+    [ testParseValid cases_valid,
+      testParseInvalid (Proxy @PID) cases_invalid
+    ]
+
+test_serialize :: TestTree
+test_serialize = testSerialize "serialize" cases_valid
+
+cases_valid :: [(Text, PID)]
+cases_valid =
+  [ ( "PID=0",
+      Param
+        { paramName = CaseInsensitiveUpper (symbolSing @"PID"),
+          paramValue =
+            NonEmpty.singleton $
+              PIDValue
+                { pidValueWholePart = NonEmpty.singleton 0,
+                  pidValueDecimalPart = Nothing
+                }
+        }
+    ),
+    ( "pid=1,5,10,52,200,419",
+      Param
+        { paramName = CaseInsensitiveUpper (symbolSing @"pid"),
+          paramValue =
+            PIDValue
+              { pidValueWholePart = 1 :| [],
+                pidValueDecimalPart = Nothing
+              }
+              :| [ PIDValue
+                     { pidValueWholePart = 5 :| [],
+                       pidValueDecimalPart = Nothing
+                     },
+                   PIDValue
+                     { pidValueWholePart = 1 :| [0],
+                       pidValueDecimalPart = Nothing
+                     },
+                   PIDValue
+                     { pidValueWholePart = 5 :| [2],
+                       pidValueDecimalPart = Nothing
+                     },
+                   PIDValue
+                     { pidValueWholePart = 2 :| [0, 0],
+                       pidValueDecimalPart = Nothing
+                     },
+                   PIDValue
+                     { pidValueWholePart = 4 :| [1, 9],
+                       pidValueDecimalPart = Nothing
+                     }
+                 ]
+        }
+    ),
+    ( "Pid=00,0401,0009250,0.0,0.1,1.4",
+      Param
+        { paramName = CaseInsensitiveUpper (symbolSing @"Pid"),
+          paramValue =
+            PIDValue
+              { pidValueWholePart = 0 :| [0],
+                pidValueDecimalPart = Nothing
+              }
+              :| [ PIDValue
+                     { pidValueWholePart = 0 :| [4, 0, 1],
+                       pidValueDecimalPart = Nothing
+                     },
+                   PIDValue
+                     { pidValueWholePart = 0 :| [0, 0, 9, 2, 5, 0],
+                       pidValueDecimalPart = Nothing
+                     },
+                   PIDValue
+                     { pidValueWholePart = 0 :| [],
+                       pidValueDecimalPart = Just (0 :| [])
+                     },
+                   PIDValue
+                     { pidValueWholePart = 0 :| [],
+                       pidValueDecimalPart = Just (1 :| [])
+                     },
+                   PIDValue
+                     { pidValueWholePart = 1 :| [],
+                       pidValueDecimalPart = Just (4 :| [])
+                     }
+                 ]
+        }
+    ),
+    ( "pId=12.55,63.0098,83.27000,005310.021900",
+      Param
+        { paramName = CaseInsensitiveUpper (symbolSing @"pId"),
+          paramValue =
+            PIDValue
+              { pidValueWholePart = 1 :| [2],
+                pidValueDecimalPart = Just (5 :| [5])
+              }
+              :| [ PIDValue
+                     { pidValueWholePart = 6 :| [3],
+                       pidValueDecimalPart = Just (0 :| [0, 9, 8])
+                     },
+                   PIDValue
+                     { pidValueWholePart = 8 :| [3],
+                       pidValueDecimalPart = Just (2 :| [7, 0, 0, 0])
+                     },
+                   PIDValue
+                     { pidValueWholePart = 0 :| [0, 5, 3, 1, 0],
+                       pidValueDecimalPart = Just (0 :| [2, 1, 9, 0, 0])
+                     }
+                 ]
+        }
+    )
+  ]
+
+cases_invalid :: [Text]
+cases_invalid =
+  ["PID=", "PID=1.", "PID= 1", "PID =1", "PID=1;", "PID=1:"]
+    ++ ["PID=,", "PID=1,,", "PID=,1,"]
+
+--------------------
+-- Testing functions
+--------------------
+
+testParseValid :: (Show a, Eq a, HasParser a) => [(Text, a)] -> TestTree
+testParseValid cases =
+  testCase "valid" $
+    forM_ cases $ \(text, value) ->
+      parse text @?= Just value
+
+testParseInvalid ::
+  forall a.
+  (Show a, Eq a, HasParser a) =>
+  Proxy a ->
+  [Text] ->
+  TestTree
+testParseInvalid _ cases =
+  testCase "invalid" $
+    forM_ cases $ \text ->
+      parse text @?= (Nothing :: Maybe a)
+
+testSerialize :: (HasSerializer a) => TestName -> [(Text, a)] -> TestTree
+testSerialize name cases =
+  testCase name $
+    forM_ cases $ \(text, value) ->
+      serialize value @?= text
