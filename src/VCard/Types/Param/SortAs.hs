e9453bd1499512105e6e1bd15a83f44e69fc2c65
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-03-18 13:08:29 -0700

    feat: Add `SortAs` property param (#125)

---
+++ src/VCard/Types/Param/SortAs.hs
@@ -0,0 +1,26 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module VCard.Types.Param.SortAs
+  ( SortAs,
+  )
+where
+
+import Data.List.NonEmpty (NonEmpty)
+import Text.Megaparsec.Char (char)
+import VCard.Parse (HasParser, Parser, parser)
+import VCard.Serialize (HasSerializer, Serializer, serializer)
+import VCard.Types.Param.Generic (Param, mkParamParser, mkParamSerializer)
+import VCard.Types.Param.ParamValue (ParamValue)
+import VCard.Util (intersperseCommaNE, sepByNonEmpty)
+
+type SortAs = Param "SORT-AS" (NonEmpty ParamValue)
+
+instance HasParser SortAs where
+  parser :: Parser SortAs
+  parser = mkParamParser (sepByNonEmpty (parser @ParamValue) (char ','))
+
+instance HasSerializer SortAs where
+  serializer :: Serializer SortAs
+  serializer = mkParamSerializer (intersperseCommaNE (serializer @ParamValue))
