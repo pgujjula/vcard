c3b09cc6ba906d3a9df97f34a867b80a38c21b58
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-03-17 17:04:06 -0700

    test: Test `ParamValue` parser (#285)
    Add unit tests for the `ParamValue`, `SParamValue s`, and
    `SomeParamValue` parsers.

--- test/Test/VCard/Types/Param/ParamValue.hs
+++ test/Test/VCard/Types/Param/ParamValue.hs
@@ -4,12 +4,21 @@
 
 module Test.VCard.Types.Param.ParamValue (tests) where
 
+import Control.Monad (forM_)
 import Data.Maybe (isJust, isNothing)
+import Data.Proxy (Proxy (..))
+import Data.Text (Text)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (Assertion, assertBool, testCase, (@?=))
-import VCard.Symbol.Private (SSymbol, symbolSing, testSSymbolEquality)
+import Test.Tasty.HUnit (Assertion, assertBool, assertFailure, testCase, (@?=))
+import VCard.Parse (HasParser, parse)
+import VCard.Symbol.Private
+  ( SSymbol,
+    symbolSing,
+    testSSymbolEquality,
+  )
 import VCard.Types.Param.ParamValue
-  ( ParamValueSymbol,
+  ( ParamValue,
+    ParamValueSymbol,
     SParamValue (..),
     SomeParamValue (..),
     paramValueVal,
@@ -18,13 +27,16 @@ import VCard.Types.Param.ParamValue
     unParamValue,
   )
 
+{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
+
 tests :: TestTree
 tests =
   testGroup
     "ParamValue"
     [ test_testParamValueSymbol,
       test_paramValueVal,
-      test_someParamValueVal
+      test_someParamValueVal,
+      test_parse
     ]
 
 test_testParamValueSymbol :: TestTree
@@ -102,8 +114,207 @@ test_someParamValueVal =
     testWith (symbolSing @"\"a\"")
     testWith (symbolSing @"\"foo;\"")
 
+test_parse :: TestTree
+test_parse =
+  testGroup
+    "parse"
+    [ test_parse_ParamValue,
+      test_parse_SParamValue,
+      test_parse_SomeParamValue
+    ]
+
+test_parse_ParamValue :: TestTree
+test_parse_ParamValue =
+  testGroup
+    "ParamValue"
+    [ testParseValid cases_ParamValue_valid,
+      testParseInvalid (Proxy @ParamValue) cases_ParamValue_invalid
+    ]
+
+test_parse_SParamValue :: TestTree
+test_parse_SParamValue =
+  testCase "SParamValue" $ do
+    -- QSafe symbols
+    assertEqualSParamValue
+      (parse @(SParamValue "\"\"") "\"\"")
+      (Just (SParamValue (symbolSing @"\"\"")))
+    assertEqualSParamValue
+      (parse @(SParamValue "\"foo\"") "\"foo\"")
+      (Just (SParamValue (symbolSing @"\"foo\"")))
+    assertEqualSParamValue
+      (parse @(SParamValue "\"foo;\"") "\"foo;\"")
+      (Just (SParamValue (symbolSing @"\"foo;\"")))
+    -- Safe symbols
+    assertEqualSParamValue
+      (parse @(SParamValue "") "")
+      (Just (SParamValue (symbolSing @"")))
+    assertEqualSParamValue
+      (parse @(SParamValue "foo") "foo")
+      (Just (SParamValue (symbolSing @"foo")))
+
+    -- Cannot parse quoted symbol as unquoted, or vice versa
+    assertEqualSParamValue (parse @(SParamValue "foo") "\"foo\"") Nothing
+    assertEqualSParamValue (parse @(SParamValue "\"foo\"") "foo") Nothing
+
+    -- Cannot parse incorrectly quoted symbol
+    assertEqualSParamValue (parse @(SParamValue "foo") "foo\"") Nothing
+    assertEqualSParamValue (parse @(SParamValue "\"foo\"") "foo\"") Nothing
+    assertEqualSParamValue (parse @(SParamValue "foo") "\"foo") Nothing
+    assertEqualSParamValue (parse @(SParamValue "\"foo\"") "\"foo") Nothing
+
+    -- Cannot parse symbol without ParamValueSymbol constraint
+    assertEqualSParamValue (parse @(SParamValue "foo;") "foo;") Nothing
+
+test_parse_SomeParamValue :: TestTree
+test_parse_SomeParamValue =
+  testGroup
+    "SomeParamValue"
+    [ testParseValid cases_SomeParamValue_valid,
+      testParseInvalid (Proxy @SomeParamValue) cases_SomeParamValue_invalid
+    ]
+
+cases_ParamValue_valid :: [(Text, ParamValue)]
+cases_ParamValue_valid =
+  [ --
+    -- QSafe
+    --
+    ( "\"\"",
+      paramValueVal (SParamValue (symbolSing @"\"\""))
+    ),
+    ( "\"a\"",
+      paramValueVal (SParamValue (symbolSing @"\"a\""))
+    ),
+    ( "\"Foo\"",
+      paramValueVal (SParamValue (symbolSing @"\"Foo\""))
+    ),
+    ( "\" \t\"",
+      paramValueVal (SParamValue (symbolSing @"\" \t\""))
+    ),
+    ( "\":;\"",
+      paramValueVal (SParamValue (symbolSing @"\":;\""))
+    ),
+    ( "\"!#\'$0123abcABC|}~\"",
+      paramValueVal (SParamValue (symbolSing @"\"!#\'$0123abcABC|}~\""))
+    ),
+    ( "\"€é‰\"",
+      paramValueVal (SParamValue (symbolSing @"\"€é‰\""))
+    ),
+    --
+    -- Safe
+    --
+    ( "",
+      paramValueVal (SParamValue (symbolSing @""))
+    ),
+    ( "a",
+      paramValueVal (SParamValue (symbolSing @"a"))
+    ),
+    ( "Foo",
+      paramValueVal (SParamValue (symbolSing @"Foo"))
+    ),
+    ( " \t",
+      paramValueVal (SParamValue (symbolSing @" \t"))
+    ),
+    ( "!#$\'0123abcABC|}~",
+      paramValueVal (SParamValue (symbolSing @"!#$\'0123abcABC|}~"))
+    ),
+    ( "€é‰",
+      paramValueVal (SParamValue (symbolSing @"€é‰"))
+    )
+  ]
+
+cases_SomeParamValue_valid :: [(Text, SomeParamValue)]
+cases_SomeParamValue_valid =
+  [ --
+    -- QSafe
+    --
+    ( "\"\"",
+      SomeParamValue (SParamValue (symbolSing @"\"\""))
+    ),
+    ( "\"a\"",
+      SomeParamValue (SParamValue (symbolSing @"\"a\""))
+    ),
+    ( "\"Foo\"",
+      SomeParamValue (SParamValue (symbolSing @"\"Foo\""))
+    ),
+    ( "\" \t\"",
+      SomeParamValue (SParamValue (symbolSing @"\" \t\""))
+    ),
+    ( "\":;\"",
+      SomeParamValue (SParamValue (symbolSing @"\":;\""))
+    ),
+    ( "\"!#\'$0123abcABC|}~\"",
+      SomeParamValue (SParamValue (symbolSing @"\"!#\'$0123abcABC|}~\""))
+    ),
+    ( "\"€é‰\"",
+      SomeParamValue (SParamValue (symbolSing @"\"€é‰\""))
+    ),
+    --
+    -- Safe
+    --
+    ( "",
+      SomeParamValue (SParamValue (symbolSing @""))
+    ),
+    ( "a",
+      SomeParamValue (SParamValue (symbolSing @"a"))
+    ),
+    ( "Foo",
+      SomeParamValue (SParamValue (symbolSing @"Foo"))
+    ),
+    ( " \t",
+      SomeParamValue (SParamValue (symbolSing @" \t"))
+    ),
+    ( "!#$\'0123abcABC|}~",
+      SomeParamValue (SParamValue (symbolSing @"!#$\'0123abcABC|}~"))
+    ),
+    ( "€é‰",
+      SomeParamValue (SParamValue (symbolSing @"€é‰"))
+    )
+  ]
+
+cases_ParamValue_invalid :: [Text]
+cases_ParamValue_invalid =
+  [ "\"foo\nbar\"",
+    "\"foo\"bar\"",
+    "\"foobar",
+    "foobar\"",
+    "foo;bar",
+    "foo:bar",
+    "foo\nbar",
+    "foo\"bar"
+  ]
+
+cases_SomeParamValue_invalid :: [Text]
+cases_SomeParamValue_invalid = cases_ParamValue_invalid
+
 -- Utilities
 assertEqualSSymbol :: SSymbol a -> SSymbol b -> Assertion
 assertEqualSSymbol sa sb =
   assertBool "expected equal SSymbols" $
     isJust (testSSymbolEquality sa sb)
+
+assertEqualSParamValue ::
+  Maybe (SParamValue a) -> Maybe (SParamValue b) -> Assertion
+assertEqualSParamValue Nothing Nothing = pure ()
+assertEqualSParamValue (Just _) Nothing =
+  assertFailure "expected equal SParamValues"
+assertEqualSParamValue Nothing (Just _) =
+  assertFailure "expected equal SParamValues"
+assertEqualSParamValue (Just (SParamValue sa)) (Just (SParamValue sb)) =
+  assertEqualSSymbol sa sb
+
+testParseValid :: (Show a, Eq a, HasParser a) => [(Text, a)] -> TestTree
+testParseValid cases =
+  testCase "valid" $
+    forM_ cases $ \(text, value) ->
+      parse text @?= Just value
+
+testParseInvalid ::
+  forall a.
+  (Show a, Eq a, HasParser a) =>
+  Proxy a ->
+  [Text] ->
+  TestTree
+testParseInvalid _ cases =
+  testCase "invalid" $
+    forM_ cases $ \text ->
+      parse text @?= (Nothing :: Maybe a)
