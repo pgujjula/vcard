6be66e62f94d7483f6f734cc4574ea7247974e89
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-03-18 17:50:23 -0700

    test: Add tests for `XName` data types (#299)

--- test/Test/VCard/XName.hs
+++ test/Test/VCard/XName.hs
@@ -1,21 +1,29 @@
 -- SPDX-FileCopyrightText: Copyright Preetham Gujjula
 -- SPDX-License-Identifier: BSD-3-Clause
+{-# LANGUAGE OverloadedStrings #-}
 
 module Test.VCard.XName (tests) where
 
 import Data.Maybe (isJust, isNothing)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (Assertion, assertBool, testCase)
+import Test.Tasty.HUnit (Assertion, assertBool, testCase, (@?=))
 import Test.VCard.XName.TypeTests
   ( test_XNameLowerSymbol,
     test_XNameSymbol,
     test_XNameUpperSymbol,
   )
+import VCard.Parse (parse)
+import VCard.Serialize (serialize)
 import VCard.Symbol.Private (symbolSing)
 import VCard.XName
-  ( testXNameLowerSymbol,
+  ( SXName (..),
+    SomeXName (..),
+    XName (..),
+    someXNameVal,
+    testXNameLowerSymbol,
     testXNameSymbol,
     testXNameUpperSymbol,
+    xNameVal,
   )
 
 tests :: TestTree
@@ -24,6 +32,11 @@ tests =
     "XName"
     [ test_XNameSymbol,
       test_testXNameSymbol,
+      test_XName,
+      test_SXName,
+      test_SomeXName,
+      test_xNameVal,
+      test_someXNameVal,
       test_XNameLowerSymbol,
       test_testXNameLowerSymbol,
       test_XNameUpperSymbol,
@@ -73,6 +86,137 @@ test_testXNameSymbol =
     assertIsNothing (testXNameSymbol (symbolSing @"x-a "))
     assertIsNothing (testXNameSymbol (symbolSing @"x- a"))
     assertIsNothing (testXNameSymbol (symbolSing @"x-é"))
+
+test_XName :: TestTree
+test_XName =
+  testGroup
+    "XName"
+    [ test_XName_parse,
+      test_XName_serialize
+    ]
+
+-- TODO: Factor out the common test cases between parse and serialize
+test_XName_parse :: TestTree
+test_XName_parse =
+  testCase "parse" $ do
+    parse @XName "x-a" @?= Just (xNameVal (SXName (symbolSing @"x-a")))
+    parse @XName "x-foo" @?= Just (xNameVal (SXName (symbolSing @"x-foo")))
+    parse @XName "X-Foo" @?= Just (xNameVal (SXName (symbolSing @"X-Foo")))
+    parse @XName "x--FOO-bar13-14--"
+      @?= Just (xNameVal (SXName (symbolSing @"x--FOO-bar13-14--")))
+
+    parse @XName "x-" @?= Nothing
+    parse @XName "X-" @?= Nothing
+    parse @XName "X-abc!" @?= Nothing
+    parse @XName "abc" @?= Nothing
+    parse @XName "xabc" @?= Nothing
+
+test_XName_serialize :: TestTree
+test_XName_serialize =
+  testCase "serialize" $ do
+    serialize
+      (xNameVal (SXName (symbolSing @"x-a")))
+      @?= "x-a"
+    serialize (xNameVal (SXName (symbolSing @"x-foo")))
+      @?= "x-foo"
+    serialize (xNameVal (SXName (symbolSing @"X-Foo")))
+      @?= "X-Foo"
+    serialize (xNameVal (SXName (symbolSing @"x--FOO-bar13-14--")))
+      @?= "x--FOO-bar13-14--"
+
+test_SXName :: TestTree
+test_SXName =
+  testGroup
+    "SXName"
+    [ test_SXName_parse,
+      test_SXName_serialize
+    ]
+
+test_SXName_parse :: TestTree
+test_SXName_parse =
+  testCase "parse" $ do
+    parse @(SXName "x-a") "x-a" @?= Just (SXName (symbolSing @"x-a"))
+    parse @(SXName "x-foo") "x-foo" @?= Just (SXName (symbolSing @"x-foo"))
+    parse @(SXName "X-Foo") "X-Foo" @?= Just (SXName (symbolSing @"X-Foo"))
+    parse @(SXName "x--FOO-bar13-14--") "x--FOO-bar13-14--"
+      @?= Just (SXName (symbolSing @"x--FOO-bar13-14--"))
+
+    parse @(SXName "x-") "x-" @?= Nothing
+    parse @(SXName "X-") "X-" @?= Nothing
+    parse @(SXName "X-abc!") "X-abc!" @?= Nothing
+    parse @(SXName "abc") "abc" @?= Nothing
+    parse @(SXName "xabc") "xabc" @?= Nothing
+
+    parse @(SXName "x-a") "x-A" @?= Nothing
+    parse @(SXName "x-a") "X-a" @?= Nothing
+    parse @(SXName "X-A") "x-A" @?= Nothing
+    parse @(SXName "X-A") "X-a" @?= Nothing
+
+test_SXName_serialize :: TestTree
+test_SXName_serialize =
+  testCase "serialize" $ do
+    serialize (SXName (symbolSing @"x-a"))
+      @?= "x-a"
+    serialize (SXName (symbolSing @"x-foo"))
+      @?= "x-foo"
+    serialize (SXName (symbolSing @"X-Foo"))
+      @?= "X-Foo"
+    serialize (SXName (symbolSing @"x--FOO-bar13-14--"))
+      @?= "x--FOO-bar13-14--"
+
+test_SomeXName :: TestTree
+test_SomeXName =
+  testGroup
+    "SomeXName"
+    [ test_SomeXName_parse,
+      test_SomeXName_serialize
+    ]
+
+test_SomeXName_parse :: TestTree
+test_SomeXName_parse =
+  testCase "parse" $ do
+    parse @SomeXName "x-a" @?= Just (SomeXName (SXName (symbolSing @"x-a")))
+    parse @SomeXName "x-foo" @?= Just (SomeXName (SXName (symbolSing @"x-foo")))
+    parse @SomeXName "X-Foo" @?= Just (SomeXName (SXName (symbolSing @"X-Foo")))
+    parse @SomeXName "x--FOO-bar13-14--"
+      @?= Just (SomeXName (SXName (symbolSing @"x--FOO-bar13-14--")))
+
+    parse @SomeXName "x-" @?= Nothing
+    parse @SomeXName "X-" @?= Nothing
+    parse @SomeXName "X-abc!" @?= Nothing
+    parse @SomeXName "abc" @?= Nothing
+    parse @SomeXName "xabc" @?= Nothing
+
+test_SomeXName_serialize :: TestTree
+test_SomeXName_serialize =
+  testCase "serialize" $ do
+    serialize
+      (SomeXName (SXName (symbolSing @"x-a")))
+      @?= "x-a"
+    serialize (SomeXName (SXName (symbolSing @"x-foo")))
+      @?= "x-foo"
+    serialize (SomeXName (SXName (symbolSing @"X-Foo")))
+      @?= "X-Foo"
+    serialize (SomeXName (SXName (symbolSing @"x--FOO-bar13-14--")))
+      @?= "x--FOO-bar13-14--"
+
+test_xNameVal :: TestTree
+test_xNameVal =
+  testCase "xNameVal" $ do
+    unXName (xNameVal (SXName (symbolSing @"x-a"))) @?= "x-a"
+    unXName (xNameVal (SXName (symbolSing @"X-Foo"))) @?= "X-Foo"
+    unXName (xNameVal (SXName (symbolSing @"x--FOO-bar13-14--")))
+      @?= "x--FOO-bar13-14--"
+
+test_someXNameVal :: TestTree
+test_someXNameVal =
+  testCase "someXNameVal" $ do
+    someXNameVal (xNameVal (SXName (symbolSing @"x-a")))
+      @?= SomeXName (SXName (symbolSing @"x-a"))
+    someXNameVal (xNameVal (SXName (symbolSing @"x-Foo")))
+      @?= SomeXName (SXName (symbolSing @"x-Foo"))
+    someXNameVal (xNameVal (SXName (symbolSing @"x--FOO-bar13-14--")))
+      @?= SomeXName (SXName (symbolSing @"x--FOO-bar13-14--"))
 
 test_testXNameLowerSymbol :: TestTree
 test_testXNameLowerSymbol =
