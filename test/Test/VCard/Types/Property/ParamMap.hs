1b25ac7a194e57b3545185db8ad8ec66042df89c
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-11-30 01:44:14 -0600

    test: Add tests for AnyParamTag (#328)

--- test/Test/VCard/Types/Property/ParamMap.hs
+++ test/Test/VCard/Types/Property/ParamMap.hs
@@ -6,6 +6,7 @@ module Test.VCard.Types.Property.ParamMap (tests) where
 import Data.Dependent.Map qualified as DMap
 import Data.Dependent.Sum (DSum ((:=>)))
 import Data.Finite (finite)
+import Data.Function ((&))
 import Data.Functor.Identity (Identity (..))
 import Data.GADT.Compare (GCompare (gcompare), GEq (geq), GOrdering (..))
 import Data.List.NonEmpty (NonEmpty ((:|)))
@@ -17,10 +18,13 @@ import VCard.Types.Param
   ( GenericParam (..),
     PIDParam,
     PIDValue (..),
+    ParamValue,
     PrefParam,
     PrefValue (..),
+    SParamValue (..),
     genericParamName,
     genericParamValue,
+    paramValueVal,
     pidValueDecimalPart,
     pidValueWholePart,
   )
@@ -59,25 +63,50 @@ test_EmailParamTag_geq :: TestTree
 test_EmailParamTag_geq = testCase "geq" $ do
   geq PIDParamTag PIDParamTag @?= Just Refl
   geq PIDParamTag PrefParamTag @?= Nothing
+  geq PIDParamTag (AnyParamTag (symbolSing @"X-FOO")) @?= Nothing
+
   geq PrefParamTag PIDParamTag @?= Nothing
   geq PrefParamTag PrefParamTag @?= Just Refl
+  geq PrefParamTag (AnyParamTag (symbolSing @"X-FOO")) @?= Nothing
+
+  geq (AnyParamTag (symbolSing @"X-FOO")) PIDParamTag @?= Nothing
+  geq (AnyParamTag (symbolSing @"X-FOO")) PrefParamTag @?= Nothing
+  geq (AnyParamTag (symbolSing @"X-FOO")) (AnyParamTag (symbolSing @"X-BAR"))
+    @?= Nothing
+  geq (AnyParamTag (symbolSing @"X-FOO")) (AnyParamTag (symbolSing @"X-FOO"))
+    @?= Just Refl
 
 test_EmailParamTag_gcompare :: TestTree
 test_EmailParamTag_gcompare = testCase "gcompare" $ do
   gcompare PIDParamTag PIDParamTag @?= GEQ
   gcompare PIDParamTag PrefParamTag @?= GLT
+  gcompare PIDParamTag (AnyParamTag (symbolSing @"X-FOO")) @?= GLT
+
   gcompare PrefParamTag PIDParamTag @?= GGT
   gcompare PrefParamTag PrefParamTag @?= GEQ
+  gcompare PrefParamTag (AnyParamTag (symbolSing @"X-FOO")) @?= GLT
+
+  gcompare (AnyParamTag (symbolSing @"X-FOO")) PIDParamTag @?= GGT
+  gcompare (AnyParamTag (symbolSing @"X-FOO")) PrefParamTag @?= GGT
+  gcompare (AnyParamTag (symbolSing @"X-FOO")) (AnyParamTag (symbolSing @"X-BAR")) @?= GGT
+  gcompare (AnyParamTag (symbolSing @"X-BAR")) (AnyParamTag (symbolSing @"X-FOO")) @?= GLT
+  gcompare (AnyParamTag (symbolSing @"X-FOO")) (AnyParamTag (symbolSing @"X-FOO")) @?= GEQ
 
 test_EmailParamTag_eq :: TestTree
 test_EmailParamTag_eq = testCase "(==)" $ do
   PIDParamTag == PIDParamTag @?= True
   PrefParamTag == PrefParamTag @?= True
+  AnyParamTag (symbolSing @"X-FOO") == AnyParamTag (symbolSing @"X-FOO")
+    @?= True
 
 test_EmailParamTag_compare :: TestTree
 test_EmailParamTag_compare = testCase "compare" $ do
   compare PIDParamTag PIDParamTag @?= EQ
   compare PrefParamTag PrefParamTag @?= EQ
+  compare
+    (AnyParamTag (symbolSing @"X-FOO"))
+    (AnyParamTag (symbolSing @"X-FOO"))
+    @?= EQ
 
 test_EmailParamMap :: TestTree
 test_EmailParamMap =
@@ -98,25 +127,46 @@ test_EmailParamMap_insert =
   testGroup
     "insert"
     [ testCase "single element" $ do
-        unwrap pidParam1Map
-          @?= [PIDParamTag :=> Identity pidParam1]
-
-        unwrap prefParamMap
-          @?= [PrefParamTag :=> Identity prefParam],
+        unwrap pidParam1Map @?= [PIDParamTag :=> Identity pidParam1]
+        unwrap prefParamMap @?= [PrefParamTag :=> Identity prefParam]
+        unwrap anyParamFoo1Map
+          @?= [AnyParamTag (symbolSing @"X-FOO") :=> Identity anyParamFoo1]
+        unwrap anyParamFoo2Map
+          @?= [AnyParamTag (symbolSing @"X-FOO") :=> Identity anyParamFoo2]
+        unwrap anyParamBarMap
+          @?= [AnyParamTag (symbolSing @"X-BAR") :=> Identity anyParamBar],
       testCase "multiple elements" $ do
-        let mp1 = insert PrefParamTag prefParam pidParam1Map
-            mp2 = insert PIDParamTag pidParam1 prefParamMap
+        let mp1 =
+              empty
+                & insert PIDParamTag pidParam1
+                & insert PrefParamTag prefParam
+                & insert (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo1
+                & insert (AnyParamTag (symbolSing @"X-BAR")) anyParamBar
+            mp2 =
+              empty
+                & insert (AnyParamTag (symbolSing @"X-BAR")) anyParamBar
+                & insert (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo1
+                & insert PrefParamTag prefParam
+                & insert PIDParamTag pidParam1
             result =
               [ PIDParamTag :=> Identity pidParam1,
-                PrefParamTag :=> Identity prefParam
+                PrefParamTag :=> Identity prefParam,
+                AnyParamTag (symbolSing @"X-BAR") :=> Identity anyParamBar,
+                AnyParamTag (symbolSing @"X-FOO") :=> Identity anyParamFoo1
               ]
         unwrap mp1 @?= result
         unwrap mp2 @?= result,
       testCase "overwrite elements" $
-        unwrap (insert PIDParamTag pidParam2 allParamsMap)
-          @?= [ PIDParamTag :=> Identity pidParam2,
-                PrefParamTag :=> Identity prefParam
-              ]
+        let replacedMap =
+              allParamsMap
+                & insert PIDParamTag pidParam2
+                & insert (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo2
+         in unwrap replacedMap
+              @?= [ PIDParamTag :=> Identity pidParam2,
+                    PrefParamTag :=> Identity prefParam,
+                    AnyParamTag (symbolSing @"X-BAR") :=> Identity anyParamBar,
+                    AnyParamTag (symbolSing @"X-FOO") :=> Identity anyParamFoo2
+                  ]
     ]
 
 test_EmailParamMap_delete :: TestTree
@@ -125,13 +175,25 @@ test_EmailParamMap_delete =
     "delete"
     [ testCase "empty" $ do
         delete PIDParamTag empty @?= empty
-        delete PrefParamTag empty @?= empty,
+        delete PrefParamTag empty @?= empty
+        delete (AnyParamTag (symbolSing @"X-FOO")) empty @?= empty
+        delete (AnyParamTag (symbolSing @"X-BAR")) empty @?= empty,
       testCase "singleton" $ do
         delete PIDParamTag pidParam1Map @?= empty
-        delete PrefParamTag pidParam1Map @?= pidParam1Map,
-      testCase "multiple elements" $ do
-        delete PrefParamTag allParamsMap @?= pidParam1Map
-        delete PIDParamTag allParamsMap @?= prefParamMap
+        delete PrefParamTag pidParam1Map @?= pidParam1Map
+
+        delete (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo1Map @?= empty
+        delete (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo2Map @?= empty
+        delete (AnyParamTag (symbolSing @"X-BAR")) anyParamFoo1Map @?= anyParamFoo1Map,
+      testCase "multiple elements" $
+        let deletedMap =
+              allParamsMap
+                & delete PIDParamTag
+                & delete (AnyParamTag (symbolSing @"X-FOO"))
+         in unwrap deletedMap
+              @?= [ PrefParamTag :=> Identity prefParam,
+                    AnyParamTag (symbolSing @"X-BAR") :=> Identity anyParamBar
+                  ]
     ]
 
 test_EmailParamMap_lookup :: TestTree
@@ -140,15 +202,32 @@ test_EmailParamMap_lookup =
     "lookup"
     [ testCase "empty" $ do
         lookup PrefParamTag empty @?= Nothing
-        lookup PIDParamTag empty @?= Nothing,
+        lookup PIDParamTag empty @?= Nothing
+        lookup (AnyParamTag (symbolSing @"X-FOO")) empty @?= Nothing
+        lookup (AnyParamTag (symbolSing @"X-BAR")) empty @?= Nothing,
       testCase "singleton" $ do
         lookup PIDParamTag pidParam1Map @?= Just pidParam1
         lookup PrefParamTag pidParam1Map @?= Nothing
+        lookup (AnyParamTag (symbolSing @"X-FOO")) pidParam1Map @?= Nothing
+
         lookup PIDParamTag prefParamMap @?= Nothing
-        lookup PrefParamTag prefParamMap @?= Just prefParam,
+        lookup PrefParamTag prefParamMap @?= Just prefParam
+        lookup (AnyParamTag (symbolSing @"X-FOO")) prefParamMap @?= Nothing
+
+        lookup PIDParamTag anyParamFoo1Map @?= Nothing
+        lookup PrefParamTag anyParamFoo1Map @?= Nothing
+        lookup (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo1Map @?= Just anyParamFoo1
+        lookup (AnyParamTag (symbolSing @"X-BAR")) anyParamFoo1Map @?= Nothing
+
+        lookup PIDParamTag anyParamBarMap @?= Nothing
+        lookup PrefParamTag anyParamBarMap @?= Nothing
+        lookup (AnyParamTag (symbolSing @"X-FOO")) anyParamBarMap @?= Nothing
+        lookup (AnyParamTag (symbolSing @"X-BAR")) anyParamBarMap @?= Just anyParamBar,
       testCase "multiple elements" $ do
         lookup PIDParamTag allParamsMap @?= Just pidParam1
         lookup PrefParamTag allParamsMap @?= Just prefParam
+        lookup (AnyParamTag (symbolSing @"X-FOO")) allParamsMap @?= Just anyParamFoo1
+        lookup (AnyParamTag (symbolSing @"X-BAR")) allParamsMap @?= Just anyParamBar
     ]
 
 --
@@ -186,14 +265,55 @@ prefParam =
       genericParamValue = PrefValue (finite 10)
     }
 
+anyParamFoo1 :: GenericParam "X-FOO" (NonEmpty ParamValue)
+anyParamFoo1 =
+  GenericParam
+    { genericParamName = CaseInsensitiveUpper (symbolSing @"X-FOO"),
+      genericParamValue =
+        NonEmpty.singleton (paramValueVal (SParamValue (symbolSing @"lorem")))
+    }
+
+anyParamFoo2 :: GenericParam "X-FOO" (NonEmpty ParamValue)
+anyParamFoo2 =
+  GenericParam
+    { genericParamName = CaseInsensitiveUpper (symbolSing @"x-foo"),
+      genericParamValue =
+        NonEmpty.singleton (paramValueVal (SParamValue (symbolSing @"ipsum")))
+    }
+
+anyParamBar :: GenericParam "X-BAR" (NonEmpty ParamValue)
+anyParamBar =
+  GenericParam
+    { genericParamName = CaseInsensitiveUpper (symbolSing @"X-BAR"),
+      genericParamValue =
+        paramValueVal (SParamValue (symbolSing @"dolor"))
+          :| [ paramValueVal (SParamValue (symbolSing @"sit")),
+               paramValueVal (SParamValue (symbolSing @"amet"))
+             ]
+    }
+
 pidParam1Map :: EmailParamMap
 pidParam1Map = insert PIDParamTag pidParam1 empty
 
 prefParamMap :: EmailParamMap
 prefParamMap = insert PrefParamTag prefParam empty
 
+anyParamFoo1Map :: EmailParamMap
+anyParamFoo1Map = insert (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo1 empty
+
+anyParamFoo2Map :: EmailParamMap
+anyParamFoo2Map = insert (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo2 empty
+
+anyParamBarMap :: EmailParamMap
+anyParamBarMap = insert (AnyParamTag (symbolSing @"X-BAR")) anyParamBar empty
+
 allParamsMap :: EmailParamMap
-allParamsMap = insert PrefParamTag prefParam pidParam1Map
+allParamsMap =
+  empty
+    & insert PIDParamTag pidParam1
+    & insert PrefParamTag prefParam
+    & insert (AnyParamTag (symbolSing @"X-FOO")) anyParamFoo1
+    & insert (AnyParamTag (symbolSing @"X-BAR")) anyParamBar
 
 --
 -- Utilities
